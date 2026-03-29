3d55b2b2e7992ee044d6b6296be8d26bd007b4b8
is-leeroy-jenkins <terry.eppler@gmail.com>
2026-03-29 18:58:33 -0400

    update

--- app.py
+++ app.py
@@ -666,7 +666,7 @@ if not use_fallback:
 			)
 
 df = load_data( )
-float_cols = df.select_dtypes( include=[ np.number ] ).columns.tolist( )
+float_cols = df.select_dtypes( include=[ np.float32 ] ).columns.tolist( )
 int_cols = df.select_dtypes( include=[ np.integer ] ).columns.tolist( )
 bool_cols = df.select_dtypes( include=[ bool ] ).columns.tolist( )
 
